9c341f352912eb1279caf748cce05a9ffbf471d0
student <stu@g.com>
2020-02-12 06:25:12 +0530

    Merge branch 'master' of https://github.com/rosetomy23/FingerprintProject
     Conflicts:
    	db.sqlite3

--- uploadapp/forms.py
+++ uploadapp/forms.py
@@ -9,7 +9,14 @@ class StudentForm(forms.Form):
         ("4", "IT"),
         ("5", "MECH-ALPHA"),
     )
+    subjects = (
+        ("1", "CSE306"),
+        ("2", "CSE409"),
+        ("3", "CSE105"),
+        ("4", "CSE104"),
+        ("5", "CSE107"),
+    )
     classname = forms.ChoiceField(choices=classes)
     date = forms.DateField()
     hour = forms.IntegerField(label="Enter the hour", min_value=1, max_value=7)
-    subcode = forms.CharField()
+    subcode = forms.ChoiceField(choices=subjects)
